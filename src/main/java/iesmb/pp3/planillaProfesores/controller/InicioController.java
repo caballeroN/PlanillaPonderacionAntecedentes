f3890eadbdcec8825de962bd7e080eac366e6fdb
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2023-11-18 02:13:07 -0300

    emparchar

--- src/main/java/iesmb/pp3/planillaProfesores/controller/InicioController.java
+++ src/main/java/iesmb/pp3/planillaProfesores/controller/InicioController.java
@@ -10,9 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/inicio")
